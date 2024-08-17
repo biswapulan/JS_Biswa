6a7b5b4de1d1613ef3e2a1768f31fa16671ddced
Biswajit Pradhan <143693333+biswapulan@users.noreply.github.com>
2024-08-17 17:37:31 +0000

    Basic file 01 update

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "33"
+//score = 33
+//score = "33abc"   //NaN => Not a Number
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  //True  as 1 => True and 0 => False
+
+//************************Operations***************************/
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2**2)
+//console.log(2%2)
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
---
+++ 01_basics/06_nums.maths.js
@@ -0,0 +1,20 @@
+const value = 400
+// console.log(value)
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 12.3435
+// console.log(otherNumber.toPrecision(3))
+
+const newNumber = 1000000
+// console.log(newNumber.toLocaleString("en-IN"))
+
+
+//++++++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++++++++
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + 10))
